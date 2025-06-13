265bdf2af98fd9f7384bce354a87213553633c24
Sahil Bajaj <sahilbajaj0941@gmail.com>
2025-06-13 11:36:40 +0530

    hn

---
+++ tictactoe/main.py
@@ -0,0 +1,66 @@
+def print_board(board):
+    print("\n")
+    print(" " + board[0] + " | " + board[1] + " | " + board[2])
+    print("---+---+---")
+    print(" " + board[3] + " | " + board[4] + " | " + board[5])
+    print("---+---+---")
+    print(" " + board[6] + " | " + board[7] + " | " + board[8])
+    print("\n")
+
+def check_win(board, player):
+    # All possible winning combinations
+    win_combinations = [
+        (0, 1, 2), (3, 4, 5), (6, 7, 8),  # Rows
+        (0, 3, 6), (1, 4, 7), (2, 5, 8),  # Columns
+        (0, 4, 8), (2, 4, 6)              # Diagonals
+    ]
+    for combo in win_combinations:
+        if board[combo[0]] == board[combo[1]] == board[combo[2]] == player:
+            return True
+    return False
+
+def check_draw(board):
+    return " " not in board
+
+def player_move(board, player):
+    while True:
+        try:
+            move = int(input(f"Player {player}, enter your move (1-9): ")) - 1
+            if board[move] == " ":
+                board[move] = player
+                break
+            else:
+                print("That spot is already taken. Try again.")
+        except (ValueError, IndexError):
+            print("Invalid input. Enter a number between 1 and 9.")
+
+def play_game():
+    board = [" "] * 9
+    current_player = "X"
+
+    print("Welcome to Tic Tac Toe!")
+    print("Board positions are like this:")
+    print(" 1 | 2 | 3")
+    print("---+---+---")
+    print(" 4 | 5 | 6")
+    print("---+---+---")
+    print(" 7 | 8 | 9")
+
+    while True:
+        print_board(board)
+        player_move(board, current_player)
+
+        if check_win(board, current_player):
+            print_board(board)
+            print(f"🎉 Player {current_player} wins!")
+            break
+
+        if check_draw(board):
+            print_board(board)
+            print("It's a draw!")
+            break
+
+        current_player = "O" if current_player == "X" else "X"
+
+# Run the game
+play_game()
